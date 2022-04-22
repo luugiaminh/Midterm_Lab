3be6d75dfbafb6e91b717c7762aa8ccf568373c1
luugiaminh <84955058+luugiaminh@users.noreply.github.com>
2022-04-22 16:49:25 +0700

    user register & hash password

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,12 +1,17 @@
-const mongoose = require ('mongoose')
+const mongoose = require('mongoose')
 
 const goalSchema = mongoose.Schema({
-    text:{
-        type:String,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        require: true,
+        ref: 'User'
+    },
+    text: {
+        type: String,
         required: [true, 'Please add a text value']
     }
 }, {
-    timestamps:true,
+    timestamps: true,
 })
 
 module.exports = mongoose.model('Goal', goalSchema)
