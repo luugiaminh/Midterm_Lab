7a31ab9f81522c2de1e865bca6995de48b26b61c
luugiaminh <84955058+luugiaminh@users.noreply.github.com>
2022-04-22 17:06:54 +0700

    jwt token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,7 +37,8 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -49,9 +50,21 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route  POST /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({
-        message: 'Login User'
-    })
+    const { email, password } = req.body
+
+    const user = await User.findOne({email})
+
+    if (user && (await bcrypt.compare(password, user.password))){
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
 })
 
 // @decs   Get user data
@@ -62,6 +75,13 @@ const getMe = asyncHandler(async (req, res) => {
         message: 'User data display'
     })
 })
+
+//generate JWT
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET,{
+        expiresIn:'30d',
+    })
+}
 
 module.exports = {
     registerUser,
