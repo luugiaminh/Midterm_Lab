687fc93a0a62dcddbc09b8b800e74ecc09e401bd
luugiaminh <84955058+luugiaminh@users.noreply.github.com>
2022-04-24 11:30:04 +0700

    load goals

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -9,14 +9,28 @@ const createGoal = async (goalData, token) => {
             Authorization: `Bearer ${token}`
         }
     }
-
+ 
     const response = await axios.post(API_URL, goalData, config)
+
+    return response.data
+}
+
+//get goal
+const getGoals = async (token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+ 
+    const response = await axios.get(API_URL, config)
 
     return response.data
 }
 
 const goalService = {
     createGoal,
+    getGoals
 }
 
 export default goalService
